eddcabd5e36380fd4bace55850498a8289dc81db
anshikadwivedi018 <anshikad2003@gmail.com>
2024-07-16 00:02:52 +0530

    /

--- move_zeroes_leetocde283.java
+++ move_zeroes_leetocde283.java
@@ -37,3 +37,21 @@ class Solution {
         }
     }
 }
+
+
+// APPROACH 3
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[l];
+                nums[l]=temp;
+                l++;
+            }
+        }
+    }
+}
