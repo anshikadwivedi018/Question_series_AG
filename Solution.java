3375bff52893fd43d5981d6acc1b4a9e8d78a3fa
anshikadwivedi018 <anshikad2003@gmail.com>
2024-07-16 20:33:57 +0530

    /

---
+++ Solution.java
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        long mini=Long.MAX_VALUE;
+        for(int i=0;i<n-m+1;i++){
+            int c=a.get(i+m-1)-a.get(i);
+            mini=(long)Math.min(mini,c);
+        }
+        return mini;
+    }
+} {
+    
+}
