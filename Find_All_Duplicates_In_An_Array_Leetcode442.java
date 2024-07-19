a632b6142f412aa842b4072295e9132e658b8b42
anshikadwivedi018 <anshikad2003@gmail.com>
2024-07-19 13:21:36 +0530

    /

--- Find_All_Duplicates_In_An_Array_Leetcode442.java
+++ Find_All_Duplicates_In_An_Array_Leetcode442.java
@@ -14,4 +14,17 @@ class Solution {
 }
 
 
-// APPROACH 2 - 
+// APPROACH 2 - using HashSet - Time complexity- O(N) and Space complexity - O(N)
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> al=new ArrayList<>();
+        Set<Integer> se=new HashSet<>();
+        for(int i: nums){
+            if(se.contains(i))
+            al.add(i);
+            se.add(i);
+        }
+        return al;
+    }
+}
