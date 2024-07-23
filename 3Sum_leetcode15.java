66a148dde2fa9548de66a5ce115843491ab45e69
anshikadwivedi018 <anshikad2003@gmail.com>
2024-07-23 18:52:47 +0530

    /

--- 3Sum_leetcode15.java
+++ 3Sum_leetcode15.java
@@ -47,3 +47,38 @@ class Solution {
         return ans;
     }
 }
+
+// APPROACH 3 - USING TWO POINTERS - OPTIMAL
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> al=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                j++;
+                else if(sum>0)
+                k--;
+                else{
+                    List<Integer>lis =new ArrayList<>();
+                    lis.add(nums[i]);
+                    lis.add(nums[j]);
+                    lis.add(nums[k]);
+                    al.add(lis);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }
+            return al;
+    }
+}
